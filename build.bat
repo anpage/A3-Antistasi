0eb2e65d29bd490e98e3776b7b582fb973b100af
Alex Page <alex@anpage.me>
2018-07-27 22:51:20 -0400

    Keep necessary template files when building

--- build.bat
+++ build.bat
@@ -5,6 +5,8 @@ mkdir "build"
 
 xcopy /E/Y A3-Antistasi\Templates\A3-AATemplate.Altis build\Antistasi.Altis\
 xcopy /E/Y A3-Antistasi build\Antistasi.Altis\
-rmdir /S/Q build\Antistasi.Altis\Templates
+rmdir /S/Q build\Antistasi.Altis\Templates\A3-AA-BLUFORTemplate.Altis
+rmdir /S/Q build\Antistasi.Altis\Templates\A3-AATemplate.Altis
+rmdir /S/Q build\Antistasi.Altis\Templates\A3-WotPTemplate.Tanoa
 
 makepbo -P build\Antistasi.Altis\ build\Antistasi.Altis.pbo
