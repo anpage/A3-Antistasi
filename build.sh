cd97947d980964593b155c97912dc117fde1bca8
Alex Page <alex@anpage.me>
2018-08-07 18:03:13 -0400

    Revert "build.sh: Enable compression"
    This reverts commit 
    bd49e2fd8f243aaa11c21af6599f2f88152792b6.
    
    Compression was preventing the mission from initializing.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cp -r A3-Antistasi/Templates/A3-AATemplate.Altis build/Antistasi.Altis
 cp -r A3-Antistasi/* build/Antistasi.Altis
 rm -rf build/Antistasi.Altis/Templates/*/
 
-makepbo -P -X none -Z default build/Antistasi.Altis/ build/Antistasi.Altis.pbo
+makepbo -P -X none build/Antistasi.Altis/ build/Antistasi.Altis.pbo
 
 rm -rf build/Antistasi.Altis/
 
@@ -18,7 +18,7 @@ cp -r A3-Antistasi/Templates/A3-AA-BLUFORTemplate.Altis build/Antistasi.BLUFOR.A
 cp -r A3-Antistasi/* build/Antistasi.BLUFOR.Altis
 rm -rf build/Antistasi.BLUFOR.Altis/Templates/*/
 
-makepbo -P -X none -Z default build/Antistasi.BLUFOR.Altis/ build/Antistasi.BLUFOR.Altis.pbo
+makepbo -P -X none build/Antistasi.BLUFOR.Altis/ build/Antistasi.BLUFOR.Altis.pbo
 
 rm -rf build/Antistasi.BLUFOR.Altis/
 
@@ -27,6 +27,6 @@ cp -r A3-Antistasi/Templates/A3-WotPTemplate.Tanoa build/Antistasi.Tanoa
 cp -r A3-Antistasi/* build/Antistasi.Tanoa
 rm -rf build/Antistasi.Tanoa/Templates/*/
 
-makepbo -P -X none -Z default build/Antistasi.Tanoa/ build/Antistasi.Tanoa.pbo
+makepbo -P -X none build/Antistasi.Tanoa/ build/Antistasi.Tanoa.pbo
 
 rm -rf build/Antistasi.Tanoa/
